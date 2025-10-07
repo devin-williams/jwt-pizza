5f77e101bc600e8b5ba339a0dd38c7f3b5007cb7
Devin Williams <dcw003@byu.edu>
2025-10-07 13:36:28 -0600

    changed the workflow

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import istanbul from 'vite-plugin-istanbul';
+import { defineConfig } from "vite";
+import istanbul from "vite-plugin-istanbul";
 
 export default defineConfig(() => ({
   plugins: [
     istanbul({
-      include: 'src/*',
-      exclude: ['node_modules', 'tests/'],
-      extension: ['.js', '.jsx', '.ts', '.tsx'],
+      include: "src/*",
+      exclude: ["node_modules", "tests/"],
+      extension: [".js", ".jsx", ".ts", ".tsx"],
       requireEnv: false,
-      forceBuildInstrument: process.env.VITE_COVERAGE === 'true',
+      forceBuildInstrument:
+        process.env.CI === "true" || process.env.VITE_COVERAGE === "true",
     }),
   ],
   server: {
